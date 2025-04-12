bb2acf4cf15372e794bc96e1eb600db91c880bc9
simba <simbarashe.makahamadze@gmail.com>
2025-04-12 08:47:04 +0200

    Add new image. Use new image in Philosophy section.

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -165,7 +165,7 @@ const Index = () => {
               </div>
               <div className="aspect-square overflow-hidden rounded-lg">
                 <img
-                  src="https://images.unsplash.com/photo-1493663284031-b7e3aefcae8e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+                  src="public/images/philosophy__image.jpeg"
                   alt="Minimalist workspace"
                   className="w-full h-full object-cover"
                 />
